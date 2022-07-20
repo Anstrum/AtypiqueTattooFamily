b7511fad5fba83b9ffb0bd5b5182c2576101c048
Anstrum <nanaka.kumiho@gmail.com>
2022-07-20 15:40:35 +0200

    Adding qr code and nav

--- init.js
+++ init.js
@@ -1,5 +1,19 @@
 const overLogo = document.querySelector("#over-logo")
 const logo = document.querySelector(".background-logo")
 
-overLogo.style.height = logo.offsetHeight + "px"
-overLogo.style.width = logo.offsetWidth + "px"
+const ninjeaQR = document.querySelector("#ninjea-qrcode")
+const liloQR = document.querySelector("#lilo-qrcode")
+
+editBaseStyle();
+
+
+ninjeaQR.addEventListener("click", () => location.href = "https://www.instagram.com/ninejea_tattoo/")
+liloQR.addEventListener("click", () => location.href = "https://www.instagram.com/lilo.tatts/")
+
+function editBaseStyle()
+{
+    overLogo.style.height = logo.offsetHeight + "px"
+    overLogo.style.width = logo.offsetWidth + "px"
+    setTimeout(() => editBaseStyle(), (10));
+}
+
